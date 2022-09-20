bec7baadc5c786bfbd4ab1e9d3858ef4be254f98
VitorVilla <vivilla1010@gmail.com>
2022-09-20 19:53:32 -0300

    TCC

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.carrocel-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 4000
+  });
